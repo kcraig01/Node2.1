59f0a7bd78d9e4b12bc7535d32b243116b321587
Kerry <kcraig01@gmail.com>
2013-10-16 12:33:24 -0600

    Jade exercise - completed requirements of exercise but still updating for practice

---
+++ app.js
@@ -0,0 +1,53 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+// app.get('/users', user.list);
+app.get('/', function(req, res){
+	res.render('index',{
+		title: "Buckwild!!!",
+		type: "text",
+		name: "Kerry",
+		"food":[
+			'salad',
+			'cookies',
+			'sandwich',
+			'soup'
+		],
+		numbers:[
+			1,
+			2,
+			3,
+			7
+		]
+	})
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
